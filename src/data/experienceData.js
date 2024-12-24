1262cbbe227780e6d3fa320944a05db89891fb64
Ullas Reddy <ullasreddy3663@gmail.com>
2024-12-24 09:23:22 -0500

    Edited data

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -11,13 +11,20 @@ export const experienceData = [
         company: 'Under Prof. Dr. Shamsad Parvin',
         jobtitle: 'Research Student',
         startYear: '2024/Jan',
-        endYear: '2024/May'
+        endYear: '2024/Jun'
     },
     {
         id: 3,
         company: ' Workspot Inc',
         jobtitle: 'Software Engineer L1',
         startYear: '2022/Aug',
-        endYear: '2023/Jul'
+        endYear: '2023/Aug'
     },
+    {
+        id: 4,
+        company: 'LeanPro',
+        jobtitle: 'Software Engineer',
+        startYear: '2021/May',
+        endYear: '2022/Aug'
+    }
 ]
